742efacef27f42c6371e1d8297937a1a1249ee78
Dino Wei <idreamer@gmail.com>
2018-10-31 15:54:39 +0800

    新增資料模型 __str__

--- default/models.py
+++ default/models.py
@@ -8,6 +8,9 @@ class Poll(models.Model):
     # 投票建立日期，在建立時若未指定，則自動填入建立時的時間
     date_created = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.id) + ")" + self.subject
+
 class Option(models.Model):
     # 此選項屬於哪一個投票
     poll_id = models.IntegerField()
@@ -15,3 +18,6 @@ class Option(models.Model):
     title = models.CharField(max_length=200)
     # 此選項被投票數
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.poll_id)+ ": " + str(self.id) + ")" + self.title
