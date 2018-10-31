c5f18695e642b1886d9ca8b9e734b867791f61ed
Dino Wei <idreamer@gmail.com>
2018-10-31 15:23:08 +0800

    新增資料模型

--- default/models.py
+++ default/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+## Create your models here.
+
+class Poll(models.Model):
+    # 投票主題文字，至多 200 字
+    subject = models.CharField(max_length=200)
+    # 投票建立日期，在建立時若未指定，則自動填入建立時的時間
+    date_created = models.DateField(auto_now_add=True)
+
+class Option(models.Model):
+    # 此選項屬於哪一個投票
+    poll_id = models.IntegerField()
+    # 選項文字
+    title = models.CharField(max_length=200)
+    # 此選項被投票數
+    count = models.IntegerField(default=0)
